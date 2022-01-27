4e9b6fea40549887ebd8a683b00dfe9990620eec
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2022-01-27 20:37:46 +0900

    add sample

--- sample/choreonoid_viewer_sample_lib.cpp
+++ sample/choreonoid_viewer_sample_lib.cpp
@@ -1,6 +1,7 @@
 #include <choreonoid_viewer/choreonoid_viewer.h>
 #include <cnoid/Body>
 #include <cnoid/BodyLoader>
+#include <cnoid/SceneMarkers>
 #include <thread>
 #include <iostream>
 #include "choreonoid_viewer_sample_lib.h"
@@ -8,13 +9,16 @@
 int mymain(){
   cnoid::BodyLoader bodyLoader;
   cnoid::BodyPtr robot = bodyLoader.load("/opt/ros/melodic/share/openhrp3/share/OpenHRP-3.1/sample/model/sample1.wrl");
+  cnoid::SphereMarkerPtr marker = new cnoid::SphereMarker(0.3, cnoid::Vector3f(1.0,0.0,0.0));
 
   choreonoid_viewer::Viewer viewer;
   viewer.objects(robot);
+  viewer.drawOn(marker);
 
   for(int i=0;i<100;i++){
     robot->joint(0)->q() = 0.01 * i;
     robot->calcForwardKinematics();
+    marker->translation()[0] = 0.01 * i;
     viewer.drawObjects();
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
